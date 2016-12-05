c34f2f8ea70c5db2f1a51b056bc4f2f21bde1c96
Ross Newman <ross.newman@abaco.com>
2016-12-05 15:01:47 +0000

    Added SDL dev dependancies

--- CMakePreBuild.sh
+++ CMakePreBuild.sh
@@ -14,7 +14,7 @@ echo "[Pre-build]  build root directory:       $BUILD_ROOT"
 
 # install packages
 sudo apt-get update
-sudo apt-get install -y libqt4-dev qt4-dev-tools libglew-dev glew-utils libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libglib2.0-dev
+sudo apt-get install -y libqt4-dev qt4-dev-tools libglew-dev glew-utils libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libglib2.0-dev libsdl2-dev libsdl2-ttf-dev
 # libgstreamer0.10-0-dev libgstreamer-plugins-base0.10-dev libxml2-dev
 sudo apt-get update
 
