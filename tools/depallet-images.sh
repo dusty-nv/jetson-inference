8f6120d7682d90ddb7105f4c840879319d7558d6
dusty-nv <dustinf@nvidia.com>
2017-03-04 18:53:18 -0500

    added image depalletization script for segmentation datasets with palletized PNG iamges

--- tools/depallet-images.sh
+++ tools/depallet-images.sh
@@ -8,7 +8,7 @@ FILES="$SEQ_IN/*.png"
 for f in $FILES
 do
 	echo "Processing $f"
-	convert $f -filter point -resize $FACTOR% -define png:color-type=2 $f
+	convert $f -define png:color-type=2 $f
 done
 
 
