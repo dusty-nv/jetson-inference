ed2771615b03ebcee3716c68d05ea47d72893bfb
dusty-nv <dustinf@nvidia.com>
2017-06-23 15:26:56 +0000

    added segnet-camera program

---
+++ segnet-camera/segnet-camera.cpp
@@ -0,0 +1,207 @@
+/*
+ * http://github.com/dusty-nv/jetson-inference
+ */
+
+#include "gstCamera.h"
+
+#include "glDisplay.h"
+#include "glTexture.h"
+
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+#include "cudaMappedMemory.h"
+#include "cudaNormalize.h"
+#include "cudaFont.h"
+
+#include "detectNet.h"
+
+
+#define DEFAULT_CAMERA -1	// -1 for onboard camera, or change to index of /dev/video V4L2 camera (>=0)	
+		
+
+bool signal_recieved = false;
+
+void sig_handler(int signo)
+{
+	if( signo == SIGINT )
+	{
+		printf("received SIGINT\n");
+		signal_recieved = true;
+	}
+}
+
+
+int main( int argc, char** argv )
+{
+	printf("segnet-camera\n  args (%i):  ", argc);
+
+	for( int i=0; i < argc; i++ )
+		printf("%i [%s]  ", i, argv[i]);
+		
+	printf("\n\n");
+	
+	
+	if( signal(SIGINT, sig_handler) == SIG_ERR )
+		printf("\ncan't catch SIGINT\n");
+
+
+	/*
+	 * create the camera device
+	 */
+	gstCamera* camera = gstCamera::Create(DEFAULT_CAMERA);
+	
+	if( !camera )
+	{
+		printf("\nsegnet-camera:  failed to initialize video device\n");
+		return 0;
+	}
+	
+	printf("\nsegnet-camera:  successfully initialized video device\n");
+	printf("    width:  %u\n", camera->GetWidth());
+	printf("   height:  %u\n", camera->GetHeight());
+	printf("    depth:  %u (bpp)\n\n", camera->GetPixelDepth());
+	
+
+	/*
+	 * create segNet
+	 */
+	segNet* net = segNet::Create(argc, argv);
+	
+	if( !net )
+	{
+		printf("segnet-camera:   failed to initialize imageNet\n");
+		return 0;
+	}
+
+	// set alpha blending value for classes that don't explicitly already have an alpha	
+	net->SetGlobalAlpha(120);
+
+	// allocate segmentation overlay output buffer
+	float* outCPU  = NULL;
+	float* outCUDA = NULL;
+
+	if( !cudaAllocMapped((void**)&outCPU, (void**)&outCUDA, camera->GetWidth() * camera->GetHeight() * sizeof(float) * 4) )
+	{
+		printf("segnet-camera:  failed to allocate CUDA memory for output image (%ux%u)\n", camera->GetWidth(), camera->GetHeight());
+		return 0;
+	}
+
+	
+	/*
+	 * create openGL window
+	 */
+	glDisplay* display = glDisplay::Create();
+	glTexture* texture = NULL;
+	
+	if( !display ) {
+		printf("\nsegnet-camera:  failed to create openGL display\n");
+	}
+	else
+	{
+		texture = glTexture::Create(camera->GetWidth(), camera->GetHeight(), GL_RGBA32F_ARB/*GL_RGBA8*/);
+
+		if( !texture )
+			printf("segnet-camera:  failed to create openGL texture\n");
+	}
+	
+
+	/*
+	 * start streaming
+	 */
+	if( !camera->Open() )
+	{
+		printf("\nsegnet-camera:  failed to open camera for streaming\n");
+		return 0;
+	}
+	
+	printf("\nsegnet-camera:  camera open for streaming\n");
+	
+	
+	/*
+	 * processing loop
+	 */
+	float confidence = 0.0f;
+	
+	while( !signal_recieved )
+	{
+		void* imgCPU  = NULL;
+		void* imgCUDA = NULL;
+		
+		// get the latest frame
+		if( !camera->Capture(&imgCPU, &imgCUDA, 1000) )
+			printf("\nsegnet-camera:  failed to capture frame\n");
+
+		// convert from YUV to RGBA
+		void* imgRGBA = NULL;
+		
+		if( !camera->ConvertRGBA(imgCUDA, &imgRGBA) )
+			printf("segnet-camera:  failed to convert from NV12 to RGBA\n");
+
+		// process image overlay
+		if( !net->Overlay(imgCUDA, outCUDA, camera->GetWidth(), camera->GetHeight()) )
+		{
+			printf("segnet-console:  failed to process segmentation overlay.\n");
+			continue;
+		}
+		
+		// update display
+		if( display != NULL )
+		{
+			char str[256];
+			sprintf(str, "TensorRT build %x | %s | %04.1f FPS", NV_GIE_VERSION, net->HasFP16() ? "FP16" : "FP32", display->GetFPS());
+			//sprintf(str, "GIE build %x | %s | %04.1f FPS | %05.2f%% %s", NV_GIE_VERSION, net->GetNetworkName(), display->GetFPS(), confidence * 100.0f, net->GetClassDesc(img_class));
+			display->SetTitle(str);	
+	
+			// next frame in the window
+			display->UserEvents();
+			display->BeginRender();
+
+			if( texture != NULL )
+			{
+				// rescale image pixel intensities for display
+				CUDA(cudaNormalizeRGBA((float4*)outCUDA, make_float2(0.0f, 255.0f), 
+								   (float4*)outCUDA, make_float2(0.0f, 1.0f), 
+		 						   camera->GetWidth(), camera->GetHeight()));
+
+				// map from CUDA to openGL using GL interop
+				void* tex_map = texture->MapCUDA();
+
+				if( tex_map != NULL )
+				{
+					cudaMemcpy(tex_map, outCUDA, texture->GetSize(), cudaMemcpyDeviceToDevice);
+					texture->Unmap();
+				}
+
+				// draw the texture
+				texture->Render(10, 10);		
+			}
+
+			display->EndRender();
+		}
+	}
+	
+	printf("\nsegnet-camera:  un-initializing video device\n");
+	
+	
+	/*
+	 * shutdown the camera device
+	 */
+	if( camera != NULL )
+	{
+		delete camera;
+		camera = NULL;
+	}
+
+	if( display != NULL )
+	{
+		delete display;
+		display = NULL;
+	}
+	
+	printf("segnet-camera:  video device has been un-initialized.\n");
+	printf("segnet-camera:  this concludes the use of the device.\n");
+	return 0;
+}
+
