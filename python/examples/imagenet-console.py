e41302096c04587ccb3697569420759c07c0f3f3
dusty-nv <dustinf@nvidia.com>
2019-05-24 15:03:58 -0400

    Python bindings

---
+++ python/examples/imagenet-console.py
@@ -0,0 +1,73 @@
+#
+# Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+#
+
+import jetson.inference
+import jetson.utils
+
+import argparse
+
+
+# parse the command line
+parser = argparse.ArgumentParser()
+
+parser.add_argument("file_in", type=str, help="filename of the input image to process")
+parser.add_argument("file_out", type=str, default=None, nargs='?', help="filename of the output image to save")
+parser.add_argument("--network", type=str, default="googlenet", help="model to use, can be:  alexnet, googlenet, googlenet-12")
+parser.add_argument("--profile", type=bool, default=False, help="enable performance profiling and multiple runs of the model")
+parser.add_argument("--runs", type=int, default=15, help="if profiling is enabling, the number of iterations to run")
+
+opt = parser.parse_args()
+
+print(opt)
+
+# load an image (into shared CPU/GPU memory)
+img, width, height = jetson.utils.loadImageRGBA(opt.file_in)
+
+# load the recognition network
+net = jetson.inference.imageNet(opt.network)
+
+# enable model profiling
+if opt.profile is True:
+	net.EnableProfiler()
+else:
+	opt.runs = 1
+
+# run model inference
+for i in range(opt.runs):
+	print("\n/////////////////////////////////////\n// RUN {:d}\n/////////////////////////////////////".format(i))
+	
+	# classify the image
+	class_idx, confidence = net.Classify(img, width, height)
+
+	# find the object description
+	class_desc = net.GetClassDesc(class_idx)
+
+	# print out the result
+	print("image is recognized as '{:s}' (class #{:d}) with {:f}% confidence\n".format(class_desc, class_idx, confidence * 100))
+
+# overlay the result on the image
+if opt.file_out is not None:
+	font = jetson.utils.cudaFont()	
+	font.Overlay(img, img, width, height, "{:f}% {:s}".format(confidence * 100, class_desc), 10, 10, font.Green)
+	jetson.utils.saveImageRGBA(opt.file_out, img, width, height)
+
+
