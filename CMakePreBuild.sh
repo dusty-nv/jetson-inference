bb7bf049ac33203f9fa64821f6d0ff49844f13f7
dusty-nv <dustinf@nvidia.com>
2019-06-06 18:46:53 +0000

    removed Qt4 dependency

--- CMakePreBuild.sh
+++ CMakePreBuild.sh
@@ -2,7 +2,6 @@
 # this script is automatically run from CMakeLists.txt
 
 BUILD_ROOT=$PWD
-TORCH_PREFIX=$PWD/torch
 
 echo "[Pre-build]  dependency installer script running..."
 echo "[Pre-build]  build root directory:   $BUILD_ROOT"
@@ -15,7 +14,7 @@ echo " "
 
 # install packages
 sudo apt-get update
-sudo apt-get install -y libqt4-dev qt4-dev-tools libglew-dev glew-utils libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libglib2.0-dev
+sudo apt-get install -y libglew-dev glew-utils libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libglib2.0-dev
 sudo apt-get install -y libopencv-calib3d-dev libopencv-dev 
 # libgstreamer0.10-0-dev libgstreamer-plugins-base0.10-dev libxml2-dev
 sudo apt-get update
