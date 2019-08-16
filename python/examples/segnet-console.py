990aff5ada75518f27b081e80d0b7686f9f3fd22
dusty-nv <dustinf@nvidia.com>
2019-08-16 14:25:55 -0400

    added segnet-console Python example

---
+++ python/examples/segnet-console.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+#
+
+import jetson.inference
+import jetson.utils
+
+import argparse
+import ctypes
+import sys
+
+
+# parse the command line
+parser = argparse.ArgumentParser(description="Segment an image using an semantic segmentation DNN.", 
+						   formatter_class=argparse.RawTextHelpFormatter, epilog=jetson.inference.segNet.Usage())
+
+parser.add_argument("file_in", type=str, help="filename of the input image to process")
+parser.add_argument("file_out", type=str, default=None, nargs='?', help="filename of the output image to save")
+parser.add_argument("--network", type=str, default="fcn-alexnet-cityscapes-sd", help="pre-trained model to load, see below for options")
+parser.add_argument("--visualize", type=str, default="overlay", choices=["overlay", "mask"], help="visualization mode for the output image, valid values are:  'overlay' and 'mask' (default: 'overlay')")
+parser.add_argument("--filter-mode", type=str, default="linear", choices=["point", "linear"], help="filtering mode used during visualization, valid values are:  'point' and 'linear' (default: 'linear')")
+parser.add_argument("--ignore-class", type=str, default="void", help="optional name of class to ignore in the visualization results (default: 'void')")
+parser.add_argument("--profile", type=bool, default=False, help="enable performance profiling and multiple runs of the model")
+parser.add_argument("--runs", type=int, default=15, help="if profiling is enabling, the number of iterations to run")
+
+try:
+	opt, argv = parser.parse_known_args()
+except:
+	print("")
+	parser.print_help()
+	sys.exit(0)
+
+
+# load an image (into shared CPU/GPU memory)
+img, width, height = jetson.utils.loadImageRGBA(opt.file_in)
+
+# allocate the output image for the overlay/mask
+img_output = jetson.utils.cudaAllocMapped(width * height * 4 * ctypes.sizeof(ctypes.c_float))
+
+# load the segmentation network
+net = jetson.inference.segNet(opt.network, argv)
+
+# enable model profiling
+if opt.profile is True:
+	net.EnableLayerProfiler()
+else:
+	opt.runs = 1
+
+# run model inference
+for i in range(opt.runs):
+	if opt.runs > 1:
+		print("\n/////////////////////////////////////\n// RUN {:d}\n/////////////////////////////////////".format(i))
+	
+	# process the segmentation network
+	net.Process(img, width, height, opt.ignore_class)
+
+	# print out timing info
+	net.PrintProfilerTimes()
+
+# perform the visualization
+if opt.file_out is not None:
+	if opt.visualize == 'overlay':
+		net.Overlay(img_output, width, height, opt.filter_mode)
+	elif opt.visualize == 'mask':
+		net.Mask(img_output, width, height, opt.filter_mode)
+
+	jetson.utils.cudaDeviceSynchronize()
+	jetson.utils.saveImageRGBA(opt.file_out, img_output, width, height)
+
+
