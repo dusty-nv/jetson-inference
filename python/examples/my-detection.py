765202f6ac314c9f9ae04f7c3efed9a619373b6e
dusty-nv <dustinf@nvidia.com>
2019-08-30 16:28:13 -0400

    added Python example my-detection.py

---
+++ python/examples/my-detection.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+#
+
+import jetson.inference
+import jetson.utils
+
+net = jetson.inference.detectNet("ssd-mobilenet-v2")
+camera = jetson.utils.gstCamera()
+display = jetson.utils.glDisplay()
+
+while display.IsOpen():
+	img, width, height = camera.CaptureRGBA()
+	detections = net.Detect(img, width, height)
+	display.RenderOnce(img, width, height)
+	display.SetTitle("Object Detection | Network {:.0f} FPS".format(1000.0 / net.GetNetworkTime()))
