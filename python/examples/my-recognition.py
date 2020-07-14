99ea5e606d9b70cec51733757db6e2f11ca81a1d
Dustin Franklin <dustinf@nvidia.com>
2020-07-14 13:20:06 -0400

    updated examples

--- python/examples/my-recognition.py
+++ python/examples/my-recognition.py
@@ -30,17 +30,17 @@ import argparse
 # parse the command line
 parser = argparse.ArgumentParser()
 parser.add_argument("filename", type=str, help="filename of the image to process")
-parser.add_argument("--network", type=str, default="googlenet", help="model to use, can be:  googlenet, resnet-18, ect. (see --help for others)")
+parser.add_argument("--network", type=str, default="googlenet", help="model to use, can be:  googlenet, resnet-18, ect.")
 opt = parser.parse_args()
 
 # load an image (into shared CPU/GPU memory)
-img, width, height = jetson.utils.loadImageRGBA(opt.filename)
+img = jetson.utils.loadImage(opt.filename)
 
 # load the recognition network
 net = jetson.inference.imageNet(opt.network)
 
 # classify the image
-class_idx, confidence = net.Classify(img, width, height)
+class_idx, confidence = net.Classify(img)
 
 # find the object description
 class_desc = net.GetClassDesc(class_idx)
