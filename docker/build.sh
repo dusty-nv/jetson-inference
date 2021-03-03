35f896c7a3cce5cb552f3737aa295e43db7b5798
Dustin Franklin <dustinf@nvidia.com>
2021-03-02 20:04:45 -0500

    updates for L4T R32.5.0

--- docker/build.sh
+++ docker/build.sh
@@ -16,7 +16,11 @@ BASE_IMAGE=$1
 source tools/l4t-version.sh
 
 if [ -z $BASE_IMAGE ]; then
-	if [ $L4T_VERSION = "32.4.4" ]; then
+	if [ $L4T_VERSION = "32.5.1" ]; then
+		BASE_IMAGE="nvcr.io/nvidia/l4t-pytorch:r32.5.0-pth1.6-py3"
+	elif [ $L4T_VERSION = "32.5.0" ]; then
+		BASE_IMAGE="nvcr.io/nvidia/l4t-pytorch:r32.5.0-pth1.6-py3"
+	elif [ $L4T_VERSION = "32.4.4" ]; then
 		BASE_IMAGE="nvcr.io/nvidia/l4t-pytorch:r32.4.4-pth1.6-py3"
 	elif [ $L4T_VERSION = "32.4.3" ]; then
 		BASE_IMAGE="nvcr.io/nvidia/l4t-pytorch:r32.4.3-pth1.6-py3"
