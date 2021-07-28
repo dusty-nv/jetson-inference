3c8b01c0730bee12610382770c5d54ebb176b4c0
Dustin Franklin <dustinf@nvidia.com>
2021-07-28 16:58:45 -0400

    added Python example for depthNet

---
+++ python/examples/depthnet.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+#
+# Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+#
+
+import jetson.inference
+import jetson.utils
+
+import argparse
+import sys
+
+from depthnet_utils import depthBuffers
+
+# parse the command line
+parser = argparse.ArgumentParser(description="Mono depth estimation on a video/image stream using depthNet DNN.", 
+                                 formatter_class=argparse.RawTextHelpFormatter, epilog=jetson.inference.depthNet.Usage() +
+                                 jetson.utils.videoSource.Usage() + jetson.utils.videoOutput.Usage() + jetson.utils.logUsage())
+
+parser.add_argument("input_URI", type=str, default="", nargs='?', help="URI of the input stream")
+parser.add_argument("output_URI", type=str, default="", nargs='?', help="URI of the output stream")
+parser.add_argument("--network", type=str, default="fcn-mobilenet", help="pre-trained model to load, see below for options")
+parser.add_argument("--colormap", type=str, default="viridis", help="colormap to use for visualization (default is 'viridis')")
+parser.add_argument("--filter-mode", type=str, default="linear", choices=["point", "linear"], help="filtering mode used during visualization, options are:\n  'point' or 'linear' (default: 'linear')")
+
+try:
+	opt = parser.parse_known_args()[0]
+except:
+	print("")
+	parser.print_help()
+	sys.exit(0)
+
+# load the segmentation network
+net = jetson.inference.depthNet(opt.network, sys.argv)
+
+# create buffer manager
+buffers = depthBuffers()
+
+# create video sources & outputs
+input = jetson.utils.videoSource(opt.input_URI, argv=sys.argv)
+output = jetson.utils.videoOutput(opt.output_URI, argv=sys.argv)
+
+# process frames until user exits
+while True:
+    # capture the next image
+    img_input = input.Capture()
+
+    # allocate buffers for this size image
+    buffers.Alloc(img_input.shape, img_input.format)
+
+    # process the mono depth and visualize
+    net.Process(img_input, buffers.depth, opt.colormap, opt.filter_mode)
+
+    # composite the images
+    jetson.utils.cudaOverlay(img_input, buffers.composite, 0, 0)
+    jetson.utils.cudaOverlay(buffers.depth, buffers.composite, img_input.width, 0)
+
+    # render the output image
+    output.Render(buffers.composite)
+
+    # update the title bar
+    output.SetStatus("{:s} | {:s} | Network {:.0f} FPS".format(opt.network, net.GetNetworkName(), net.GetNetworkFPS()))
+
+    # print out performance info
+    jetson.utils.cudaDeviceSynchronize()
+    net.PrintProfilerTimes()
+
+    # exit on input/output EOS
+    if not input.IsStreaming() or not output.IsStreaming():
+        break
