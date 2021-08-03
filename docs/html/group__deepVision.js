83ebc3b8e2d7c5f4eb6bde46ab89992df4672b11
Dustin Franklin <dustinf@nvidia.com>
2021-08-03 15:20:17 -0400

    updated docs

--- docs/html/group__deepVision.js
+++ docs/html/group__deepVision.js
@@ -2,8 +2,8 @@ var group__deepVision =
 [
     [ "tensorNet", "group__tensorNet.html", "group__tensorNet" ],
     [ "imageNet", "group__imageNet.html", "group__imageNet" ],
+    [ "depthNet", "group__depthNet.html", "group__depthNet" ],
     [ "detectNet", "group__detectNet.html", "group__detectNet" ],
     [ "poseNet", "group__poseNet.html", "group__poseNet" ],
-    [ "segNet", "group__segNet.html", "group__segNet" ],
-    [ "depthNet", "group__depthNet.html", "group__depthNet" ]
+    [ "segNet", "group__segNet.html", "group__segNet" ]
 ];
