7a1f961bf1caeb1f29bcff24ba96fcb6bfba5d21
dusty-nv <dustinf@nvidia.com>
2021-08-09 13:11:50 -0400

    fixed default --network argument to posenet.py

--- python/examples/posenet.py
+++ python/examples/posenet.py
@@ -34,7 +34,7 @@ parser = argparse.ArgumentParser(description="Run pose estimation DNN on a video
 
 parser.add_argument("input_URI", type=str, default="", nargs='?', help="URI of the input stream")
 parser.add_argument("output_URI", type=str, default="", nargs='?', help="URI of the output stream")
-parser.add_argument("--network", type=str, default="ssd-mobilenet-v2", help="pre-trained model to load (see below for options)")
+parser.add_argument("--network", type=str, default="resnet18-body", help="pre-trained model to load (see below for options)")
 parser.add_argument("--overlay", type=str, default="links,keypoints", help="pose overlay flags (e.g. --overlay=links,keypoints)\nvalid combinations are:  'links', 'keypoints', 'boxes', 'none'")
 parser.add_argument("--threshold", type=float, default=0.15, help="minimum detection threshold to use") 
 
