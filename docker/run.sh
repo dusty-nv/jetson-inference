bbabdd7b97b80f7577c576086ef41c953fc6aa59
Bryan Pfremmer <pfremm@users.noreply.github.com>
2021-11-09 22:43:54 -0600

    Resolves seccomp error
    Disables seccomp error for updated versions of docker.

--- docker/run.sh
+++ docker/run.sh
@@ -165,7 +165,7 @@ echo "V4L2_DEVICES:  $V4L2_DEVICES"
 # run the container
 sudo xhost +si:localuser:root
 
-sudo docker run --runtime nvidia -it --rm --network host -e DISPLAY=$DISPLAY \
+sudo docker run --runtime nvidia -it --rm --security-opt  seccomp=unconfined --network host -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix/:/tmp/.X11-unix \
     -v /tmp/argus_socket:/tmp/argus_socket \
     -v /etc/enctune.conf:/etc/enctune.conf \
