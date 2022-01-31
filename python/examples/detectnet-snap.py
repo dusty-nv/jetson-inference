b75ea72e04150fbb31b335dde45f104386927e8d
dusty-nv <dustinf@nvidia.com>
2022-01-31 13:24:28 -0500

    added detection script for saving object snapshots

---
+++ python/examples/detectnet-snap.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python3
+#
+# Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+#
+# This script saves the detected objects to individual images in a directory.
+# The directory these are stored in can be set using the --snapshots argument:
+#
+#   python3 detectnet-snap.py --snapshots /path/to/snapshots <input_URI> <output_URI>
+#
+# The input and output streams are specified the same way as shown here:
+# https://github.com/dusty-nv/jetson-inference/blob/master/docs/aux-streaming.md
+#
+
+import jetson.inference
+import jetson.utils
+
+import argparse
+import datetime
+import math
+import sys
+import os
+
+# parse the command line
+parser = argparse.ArgumentParser(description="Locate objects in a live camera stream using an object detection DNN.", 
+                                 formatter_class=argparse.RawTextHelpFormatter, epilog=jetson.inference.detectNet.Usage() +
+                                 jetson.utils.videoSource.Usage() + jetson.utils.videoOutput.Usage() + jetson.utils.logUsage())
+
+parser.add_argument("input_URI", type=str, default="", nargs='?', help="URI of the input stream")
+parser.add_argument("output_URI", type=str, default="", nargs='?', help="URI of the output stream")
+parser.add_argument("--network", type=str, default="ssd-mobilenet-v2", help="pre-trained model to load (see below for options)")
+parser.add_argument("--overlay", type=str, default="box,labels,conf", help="detection overlay flags (e.g. --overlay=box,labels,conf)\nvalid combinations are:  'box', 'labels', 'conf', 'none'")
+parser.add_argument("--threshold", type=float, default=0.5, help="minimum detection threshold to use") 
+parser.add_argument("--snapshots", type=str, default="images/test/detections", help="output directory of detection snapshots")
+parser.add_argument("--timestamp", type=str, default="%Y%m%d-%H%M%S-%f", help="timestamp format used in snapshot filenames")
+
+try:
+	opt = parser.parse_known_args()[0]
+except:
+	print("")
+	parser.print_help()
+	sys.exit(0)
+
+# make sure the snapshots dir exists
+os.makedirs(opt.snapshots, exist_ok=True)
+
+# create video output object 
+output = jetson.utils.videoOutput(opt.output_URI, argv=sys.argv)
+	
+# load the object detection network
+net = jetson.inference.detectNet(opt.network, sys.argv, opt.threshold)
+
+# create video sources
+input = jetson.utils.videoSource(opt.input_URI, argv=sys.argv)
+
+
+# process frames until the user exits
+while True:
+	# capture the next image
+	img = input.Capture()
+
+	# detect objects in the image (with overlay)
+	detections = net.Detect(img, overlay=opt.overlay)
+
+	# print the detections
+	print("detected {:d} objects in image".format(len(detections)))
+    
+	timestamp = datetime.datetime.now().strftime(opt.timestamp)
+    
+	for idx, detection in enumerate(detections):
+		print(detection)
+		roi = (int(detection.Left), int(detection.Top), int(detection.Right), int(detection.Bottom))
+		snapshot = jetson.utils.cudaAllocMapped(width=roi[2]-roi[0], height=roi[3]-roi[1], format=img.format)
+		jetson.utils.cudaCrop(img, snapshot, roi)
+		jetson.utils.cudaDeviceSynchronize()
+		jetson.utils.saveImage(os.path.join(opt.snapshots, f"{timestamp}-{idx}.jpg"), snapshot)
+		del snapshot
+        
+	# render the image
+	output.Render(img)
+
+	# update the title bar
+	output.SetStatus("{:s} | Network {:.0f} FPS".format(opt.network, net.GetNetworkFPS()))
+
+	# print out performance info
+	net.PrintProfilerTimes()
+
+	# exit on input/output EOS
+	if not input.IsStreaming() or not output.IsStreaming():
+		break
+
+
