00edaec61dbd04e29de2bd0a0213818ccac90ef2
Dustin Franklin <dustinf@nvidia.com>
2022-06-13 12:15:40 -0400

    fixed L4T container versioning

--- docker/tag.sh
+++ docker/tag.sh
@@ -6,7 +6,7 @@ source docker/containers/scripts/l4t_version.sh
 if [ $ARCH = "aarch64" ]; then
 	# local container:tag name
 	CONTAINER_IMAGE="jetson-inference:r$L4T_VERSION"
-
+	
 	# incompatible L4T version
 	function version_error()
 	{
@@ -17,34 +17,19 @@ if [ $ARCH = "aarch64" ]; then
 
 	# get remote container URL
 	if [ $L4T_RELEASE -eq 32 ]; then
-		if [ $L4T_REVISION_MAJOR -eq 4 ]; then
-			if [ $L4T_REVISION_MINOR -gt 4 ]; then
-				CONTAINER_REMOTE_IMAGE="nvcr.io/ea-linux4tegra/$CONTAINER_IMAGE"
-			elif [ $L4T_REVISION_MINOR -ge 3 ]; then
-				CONTAINER_REMOTE_IMAGE="dustynv/$CONTAINER_IMAGE"
-			else
-				version_error
-			fi
-		elif [ $L4T_REVISION_MAJOR -eq 5 ]; then
-			if [ $L4T_REVISION_MINOR -eq 0 ]; then
-				CONTAINER_REMOTE_IMAGE="dustynv/$CONTAINER_IMAGE"
-			elif [ $L4T_REVISION_MINOR -eq 1 ] || [ $L4T_REVISION_MINOR -eq 2 ]; then
-				# L4T R32.5.1 / R32.5.2 runs the R32.5.0 container
-				CONTAINER_IMAGE="jetson-inference:r32.5.0"
-				CONTAINER_REMOTE_IMAGE="dustynv/$CONTAINER_IMAGE"
-			else
-				CONTAINER_REMOTE_IMAGE="nvcr.io/ea-linux4tegra/$CONTAINER_IMAGE"
-			fi
-		elif [ $L4T_REVISION_MAJOR -gt 5 ]; then
-			CONTAINER_REMOTE_IMAGE="dustynv/$CONTAINER_IMAGE"
-		else
+		if [[ $L4T_REVISION_MAJOR -lt 4 && $L4T_REVISION_MINOR -gt 4 ]]; then
+			# L4T R32.4 was the first version containers are supported on
 			version_error
+		elif [ $L4T_REVISION_MAJOR -eq 5 ]; then
+			# L4T R32.5.x all run the R32.5.0 container
+			CONTAINER_IMAGE="jetson-inference:r32.5.0"
+		elif [ $L4T_REVISION_MAJOR -eq 7 ]; then
+			# L4T R32.7.x all run the R32.7.0 container
+			CONTAINER_IMAGE="jetson-inference:r32.7.0"
 		fi
-	elif [ $L4T_RELEASE -eq 34 ]; then
-		CONTAINER_REMOTE_IMAGE="dustynv/$CONTAINER_IMAGE"
-	else
-		version_error
 	fi
+	
+	CONTAINER_REMOTE_IMAGE="dustynv/$CONTAINER_IMAGE"
 	
 elif [ $ARCH = "x86_64" ]; then
 	# TODO:  add logic here for getting the latest release
