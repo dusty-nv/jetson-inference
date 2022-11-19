3c62f24f9dbf8b7f468cc9bd0241a2ba4b5fc5a7
Dustin Franklin <dustinf@nvidia.com>
2022-11-19 00:43:00 -0500

    updated dash app

--- python/www/dash/app.py
+++ python/www/dash/app.py
@@ -3,7 +3,7 @@
 # Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
+# copy of this software and associated documentation files (the 'Software'),
 # to deal in the Software without restriction, including without limitation
 # the rights to use, copy, modify, merge, publish, distribute, sublicense,
 # and/or sell copies of the Software, and to permit persons to whom the
@@ -12,7 +12,7 @@
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
 # THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -20,68 +20,97 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 #
-
-from dash import Dash, html, dcc, Input, Output, State, ClientsideFunction
-from server import Server
-
 import argparse
 import json
+
+import dash
 import dash_bootstrap_components as dbc
 
-external_scripts = ["https://webrtc.github.io/adapter/adapter-latest.js"]
-external_stylesheets = [] #[dbc.themes.DARKLY] #[dbc.themes.SLATE] #[dbc.themes.FLATLY] #[dbc.themes.SUPERHERO] #["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+from dash import Dash, html, dcc, Input, Output, State, ALL, MATCH
+from dash.exceptions import PreventUpdate
 
-app_title = "WebRTC Dashboard"  # make this configurable
+from server import Server
+from users import Authenticate
+
+
+
+
+external_scripts = ['https://webrtc.github.io/adapter/adapter-latest.js']
+external_stylesheets = [] #[dbc.themes.DARKLY] #[dbc.themes.SLATE] #[dbc.themes.FLATLY] #[dbc.themes.SUPERHERO] #['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app_title = 'WebRTC Dashboard'  # make this configurable
+
 app = Dash(__name__, external_scripts=external_scripts, external_stylesheets=external_stylesheets, title=app_title)
+auth = Authenticate(app)
 
+
+def create_navbar(config=[]):
+    # dynamically create navbar children based on the current server config
+    if len(config) == 0 or len(config['streams']) == 0:   # empty config / default navbar
+        return [dbc.NavLink('Add Stream', id='navbar_add_stream')]
+        
+    # populate streams menu
+    stream_menu_items = [
+        dbc.DropdownMenuItem('Add Stream', id='navbar_add_stream', n_clicks=0),
+        dbc.DropdownMenuItem('Streams', header=True),
+    ] 
+    
+    stream_menu_items += [
+        dbc.DropdownMenuItem(name, id={'type': 'navbar_stream', 'index': name}) for name in config['streams']
+    ]
+        
+    return [dbc.DropdownMenu(
+        children=stream_menu_items,
+        nav=True,
+        in_navbar=True,
+        label='Streams',
+        id='navbar_streams_dropdown')]
+        
 navbar = dbc.NavbarSimple(
-    children=[
-        #dbc.NavItem(dbc.NavLink("Page 1", href="#")),
-        dbc.NavItem(dbc.NavLink("Add Stream", id="navbar_add_stream")),
-        dbc.DropdownMenu(
-            children=[
-                dbc.DropdownMenuItem("More pages", header=True),
-                dbc.DropdownMenuItem("Page 2", href="#"),
-                dbc.DropdownMenuItem("Page 3", href="#"),
-            ],
-            nav=True,
-            in_navbar=True,
-            label="More",
-        ),
-    ],
+    children=create_navbar(),
     brand=app_title,
-    #brand_href="#",
-    color="primary",
+    #brand_href='#',
+    color='primary',
+    id='navbar',
     dark=True,
 )
 
 add_stream_dialog = dbc.Modal([
-        dbc.ModalHeader(dbc.ModalTitle("Add Stream")),
+        dbc.ModalHeader(dbc.ModalTitle('Add Stream')),
         dbc.ModalBody(dbc.Form([
             html.Div([
-                dbc.Label("Stream Name", html_for="add_stream_name"),
-                dbc.Input(id="add_stream_name", value="/my_stream"), #placeholder="/my_stream"),
-                dbc.FormText("Your name of the stream (e.g. location)"),
-            ], className="mb-3"),
+                dbc.Label('Stream Name', html_for='add_stream_name'),
+                dbc.Input(id='add_stream_name', value='/my_stream'), #placeholder='/my_stream'),
+                dbc.FormText('Your name of the stream (e.g. location)'),
+            ], className='mb-3'),
             html.Div([
-                dbc.Label("Video Source", html_for="add_stream_source"),
-                dbc.Input(id="add_stream_source", value="/dev/video1"), #placeholder="/dev/video0"),
-                dbc.FormText("Camera input: /dev/video*, csi://0, rtsp://, file://"),
-            ], className="mb-3"),
+                dbc.Label('Video Source', html_for='add_stream_source'),
+                dbc.Input(id='add_stream_source', value='/dev/video0'), #placeholder='/dev/video0'),
+                dbc.FormText('Camera input: /dev/video*, csi://0, rtsp://, file://'),
+            ], className='mb-3'),
+            html.Div([
+                dbc.Checklist(
+                    options=[{'label' : 'Auto Play', 'value': 'auto_play'}],
+                    value=['auto_play'],
+                    id='add_stream_checklist'),
+            ], className='mb-3'),
         ])),
         dbc.ModalFooter(
-            dbc.Button("Create", id="add_stream_submit", className="ms-auto", n_clicks=0)
+            dbc.Button('Create', id='add_stream_submit', className='ms-auto', n_clicks=0)
         )],
-    id="add_stream_dialog",
+    id='add_stream_dialog',
     is_open=False)
         
 @app.callback(
-    Output("add_stream_dialog", "is_open"),
-    Input("navbar_add_stream", "n_clicks"), 
-    Input("add_stream_submit", "n_clicks"),
-    State("add_stream_dialog", "is_open"),
+    Output('add_stream_dialog', 'is_open'),
+    Input('navbar_add_stream', 'n_clicks'), 
+    Input('add_stream_submit', 'n_clicks'),
+    State('add_stream_dialog', 'is_open'),
 )
-def toggle_add_stream_dialog(n1, n2, is_open):
+def show_add_stream_dialog(n1, n2, is_open):
+    if n1 == 0: 
+        raise PreventUpdate
+
     if n1 or n2:
         return not is_open
     return is_open
@@ -89,48 +118,87 @@ def toggle_add_stream_dialog(n1, n2, is_open):
 app.layout = html.Div([
     navbar,
     add_stream_dialog,
-    html.Video(id="video_player", controls=True),
-    html.Div(id="status_text"),
-    dcc.Store(id="stream_config"),
-    html.Div(id="hidden_div", style={"display":"none"}),
+    html.Video(id='video_player', controls=True, autoPlay=True),
+    html.Div(id='status_text'),
+    html.Div(id='hidden_div', style={'display':'none'}),    # used as null output for playStream() javascript callback
+    dcc.Store(id='play_stream_config'),                     # used to trigger a stream being played in the video player
+    dcc.Store(id='play_stream_config_add'),                 # when a stream gets added, it triggers the callback chain with this
+    dcc.Store(id='server_config'),                          # used to store the server streams config the UI is based on
+    dcc.Interval(id='refresh_nav_timer', interval=1000)     # timer that pulls the streams config from the server and refreshes the UI
 ])
 
-@app.callback(Output("status_text", "children"),
-              Output("stream_config", "data"),
-              Input("add_stream_submit", "n_clicks"),
-              State("add_stream_name", "value"),
-              State("add_stream_source", "value"),
+@app.callback(Output('status_text', 'children'),
+              Output('play_stream_config_add', 'data'),
+              Input('add_stream_submit', 'n_clicks'),
+              State('add_stream_name', 'value'),
+              State('add_stream_source', 'value'),
+              State('add_stream_checklist', 'value'),
               prevent_initial_call=True)
-def add_stream(n_clicks, name, source):
+def add_stream(n_clicks, name, source, options):
     stream_config = server.streams.add(name, source)
     return [
         html.Div([
-            f"Adding stream:  {name}", html.Br(), 
-            f"Stream source:  {source}", html.Br(), 
-            f"{stream_config}", html.Br()]),
-        json.dumps(stream_config)]
+            f'Adding stream:  {name}', html.Br(), 
+            f'Stream source:  {source}', html.Br(), 
+            f'{stream_config}', html.Br()]),
+        json.dumps(stream_config) if 'auto_play' in options else dash.no_update]
     
 
+@app.callback(
+    Output('play_stream_config', 'data'),
+    Input({'type': 'navbar_stream', 'index': ALL}, 'n_clicks'),
+    Input('play_stream_config_add', 'data'),
+    State('server_config', 'data'))
+def play_stream(n_clicks, added_stream, server_config):
+    if isinstance(dash.ctx.triggered_id, str) and dash.ctx.triggered_id == 'play_stream_config_add':
+        return added_stream  # the 'Add Stream' dialog triggered this
+    elif not isinstance(dash.ctx.triggered_id, dict): 
+        raise PreventUpdate  # only the navbar buttons make dict id's
+    
+    if n_clicks[0] is None:
+        raise PreventUpdate
+        
+    stream_name = dash.ctx.triggered_id.index
+    server_config = json.loads(server_config)
+    
+    return json.dumps(server_config['streams'][stream_name])
+
 app.clientside_callback(
-    ClientsideFunction('webrtc', 'playStream'),
-    # specifiy the callback with ClientsideFunction(<namespace>, <function name>)
-    # the Output, Input and State are passed in as with a regular callback
-    Output('hidden_div', 'children'),
-    Input('stream_config', 'data'),
+    dash.ClientsideFunction('webrtc', 'playStream'),
+    Output('hidden_div', 'children'),  # script has no output, but dash callbacks must have outputs
+    Input('play_stream_config', 'data'),
     prevent_initial_call=True
 )
     
+@app.callback(Output('navbar', 'children'),
+              Output('server_config', 'data'),
+              Input('refresh_nav_timer', 'n_intervals'),
+              Input('server_config', 'data'))
+def refresh_nav(n_intervals, previous_config_json):
+    #print(f'refresh config {n_intervals}')
+    server_config = server.get_config()
+    #print(f'server config:  ({len(server_config["streams"])} streams)\n{server_config}')
+    
+    if previous_config_json is not None:
+        previous_config = json.loads(previous_config_json)
+        
+        if server_config == previous_config:
+            raise PreventUpdate   # if the config hasn't changed, skip the update
 
-if __name__ == "__main__":
+    print(f'received updated config from backend server')
+    return [create_navbar(server_config), json.dumps(server_config)]
+    
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     
-    parser.add_argument("--host", default="0.0.0.0", type=str)
-    parser.add_argument("--port", default=8050, type=int)
-    parser.add_argument("--rpc-port", default=49565, type=int)
-    parser.add_argument("--webrtc-port", default=49567, type=int)
-    parser.add_argument("--ssl-cert", default="", type=str)
-    parser.add_argument("--ssl-key", default="", type=str)
-    #parser.add_argument("--debug", action="store_true")
+    parser.add_argument('--host', default='0.0.0.0', type=str)
+    parser.add_argument('--port', default=8050, type=int)
+    parser.add_argument('--rpc-port', default=49565, type=int)
+    parser.add_argument('--webrtc-port', default=49567, type=int)
+    parser.add_argument('--ssl-cert', default='', type=str)
+    parser.add_argument('--ssl-key', default='', type=str)
+    #parser.add_argument('--debug', action='store_true')
     
     args = parser.parse_args()
 
@@ -139,8 +207,8 @@ if __name__ == "__main__":
     
     if args.ssl_cert and args.ssl_key:
         ssl_context = (args.ssl_cert, args.ssl_key)
-        print(f"using SSL cert: {args.ssl_cert}")
-        print(f"using SSL key:  {args.ssl_key}")
+        print(f'using SSL cert: {args.ssl_cert}')
+        print(f'using SSL key:  {args.ssl_key}')
         
     # start the backend server
     server = Server(host=args.host, rpc_port=args.rpc_port, webrtc_port=args.webrtc_port, ssl_context=ssl_context)
@@ -152,5 +220,5 @@ if __name__ == "__main__":
     app.run_server(host=args.host, port=args.port, ssl_context=ssl_context, debug=True, use_reloader=False)
     
     server.stop()
-    print("exiting app")
+    print('exiting app')
     
--- python/www/dash/server.py
+++ python/www/dash/server.py
@@ -23,13 +23,14 @@
 import threading
 import multiprocessing
 import ssl
+import time
 
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.client import ServerProxy
 
-from jetson_utils import videoSource, videoOutput
+from jetson_utils import videoSource, videoOutput, Log
 
-# TODO using logging APIs...
+
 # TODO pass videoSource/videoOutput flags  (ssl_certs, ect)
 class Stream:
     def __init__(self, server, name, source):
@@ -40,32 +41,36 @@ class Stream:
         video_args = None
         
         if server.ssl_context is not None:
-            video_args = [f'--ssl-cert={server.ssl_context[0]}', f'--ssl-key={server.ssl_context[1]}']
+            video_args = [f"--ssl-cert={server.ssl_context[0]}", f"--ssl-key={server.ssl_context[1]}"]
             
         self.server = server
         self.name = name
-        self.source_path = source
-        self.source = videoSource(self.source_path)
-        self.output_path = f'webrtc://@:{self.server.webrtc_port}{self.name}'
-        self.output = videoOutput(self.output_path, argv=video_args)
+        self.source = videoSource(source, argv=video_args)
+        self.output = videoOutput(f"webrtc://@:{self.server.webrtc_port}{self.name}", argv=video_args)
         self.frame_count = 0
         
     def process(self):
-        img = self.source.Capture()
-        
+        try:
+            img = self.source.Capture()
+        except Exception as error:
+            # TODO check if stream is still open, if not reconnect?
+            Log.Error(f"{error}")
+            return
+            
         if self.frame_count % 25 == 0 or self.frame_count < 15:
-            print(f'[{self.server.name}] {self.name} -- captured frame {self.frame_count}  ({img.width}x{img.height})')
+            Log.Verbose(f"[{self.server.name}] {self.name} -- captured frame {self.frame_count}  ({img.width}x{img.height})")
 
         self.output.Render(img)
         self.frame_count += 1
        
-    @property
-    def config(self):
+    def get_config(self):
+        # TODO add stats or runtime_stats option for easy frontend state-change comparison?
+        # the videoOptions could be dynamic as well... (i.e. framerate - actually that is not?)
         return {
-            'name' : self.name,
-            'source' : self.source.GetOptions(),
-            'output' : self.output.GetOptions(),
-            'frame_count' : self.frame_count 
+            "name" : self.name,
+            "source" : self.source.GetOptions(),
+            "output" : self.output.GetOptions(),
+            #'frame_count' : self.frame_count 
         }
         
 class Streams:
@@ -74,10 +79,10 @@ class Streams:
         self.streams = []
         
     def add(self, name, source):
-        print(f'[{self.server.name}] adding stream {name}  source {source}')
+        print(f"[{self.server.name}] adding stream {name}  source {source}")
         stream = Stream(self.server, name, source)
         self.streams.append(stream)
-        return stream.config  # return the config dict, as this is called over RPC 
+        return stream.get_config()  # return the config dict, as this is called over RPC 
         
     def keys(self):
         return [stream.name for stream in self.streams]
@@ -88,7 +93,7 @@ class Streams:
     def __getitem__(self, key):
         if isinstance(key, int):
             if key < 0 or key >= len(self.streams):
-                raise IndexError('index was out of range')
+                raise IndexError("stream index was out of range")
             return self.streams[key]
         elif isinstance(key, str):
             for stream in self.streams:
@@ -96,24 +101,26 @@ class Streams:
                     return stream
             raise KeyError(f"couldn't find stream '{key}'")
         else:
-            raise TypeError('index/key must be of type int or string')
+            raise TypeError("index/key must be of type int or string")
 
+    def get_config(self):
+        return {stream.name : stream.get_config() for stream in self.streams}
 
         
 class Server:
     """
-    Media server for handling streams, models, datasets, ect.
+    Backend inference media streaming server for handling streams, models, datasets, ect.
     It typically runs in it's own process and uses RPC for communication. 
     """
-    def __init__(self, name='webrtc-dash-server', host='0.0.0.0', rpc_port=49565, webrtc_port=49567, ssl_context=None):
+    def __init__(self, name="inference-backend", host="0.0.0.0", rpc_port=49565, webrtc_port=49567, ssl_context=None):
         self.name = name
         self.host = host
         self.rpc_port = rpc_port
         self.webrtc_port = webrtc_port
         self.ssl_context = ssl_context
-        self.run_flag = True
         self.streams = Streams(self)
-
+        self.run_flag = False  # this gets set to true when initialized successfully
+        
     def init(self):
         """
         This gets called once the beginning of run() from within the process.
@@ -127,27 +134,57 @@ class Server:
         self.rpc_server.register_instance(self, allow_dotted_names=True)
         
         # run the RPC server in it's own thread
-        self.rpc_thread = threading.Thread(target=lambda: self.rpc_server.serve_forever(), name=f'{self.name}-rpc')
-        self.rpc_thread.start()        
+        self.rpc_thread = threading.Thread(target=lambda: self.rpc_server.serve_forever(), name=f"{self.name}-rpc")
+        self.rpc_thread.start()     
         
-        print(f'[{self.name}] running RPC server @ http://{self.host}:{self.rpc_port}')
-        
+        Log.Info(f"[{self.name}] RPC server is running @ http://{self.host}:{self.rpc_port}")
+
+        # indicate that server is ready to run
+        self.run_flag = True
+
     def start(self):
         """
         Launch the server running in a new process.
         Returns the RPC proxy object for clients to call.
         """
-        multiprocessing.set_start_method('spawn')  # we don't need the dash/webserver stuff, so spawn instead of 'fork'
-        self.os_process = multiprocessing.Process(target=self.run, name=self.name)
+        # we don't need the dash/webserver stuff, so use spawn instead of fork
+        # TODO look into the memory savings/implications of this
+        # https://britishgeologicalsurvey.github.io/science/python-forking-vs-spawn/
+        multiprocessing.set_start_method("spawn")  
+        
+        # start the server process
+        self.os_process = multiprocessing.Process(target=self.run, name=self.name, daemon=True)  # use daemon=True so process automatically exits when parent process exits
         self.os_process.start()
-        self.rpc_proxy = ServerProxy(f'http://{self.host}:{self.rpc_port}')
+            
+        # create the RPC proxy object for the caller
+        self.rpc_proxy = ServerProxy(f"http://{self.host}:{self.rpc_port}")
+        
+        # rudimentary check to confirm the process started/initialized ok
+        # TODO put this in a loop and re-try multiple times for when we start loading models/ect at server start-up
+        max_retries = 10
+        retry_count = 0
+        
+        for retry_count in range(max_retries):
+            time.sleep(0.5)
+        
+            try:
+                if self.rpc_proxy.is_running():
+                    break
+                else:
+                    if retry_count == max_retries - 1:
+                        raise Exception(f"[{self.name}] failed to start running")
+            except ConnectionRefusedError as error:
+                Log.Verbose(f"[{self.name}] {error}")
+            
+            #Log.Verbose(f"[{self.name}] waiting for connection... (retry {retry_count+1} of {max_retries})")
+            
         return self.rpc_proxy
         
     def stop(self):
         """
         Signal the process to stop running.
         """
-        print(f'[{self.name}] stopping...')
+        print(f"[{self.name}] stopping...")
         self.run_flag = False
         self.rpc_server._BaseServer__shutdown_request = True 
         self.rpc_server.server_close()
@@ -162,11 +199,27 @@ class Server:
         while self.run_flag:
             self.process()
           
-        print(f'[{self.name}] stopped')
+        print(f"[{self.name}] stopped")
+        
+    def is_running(self):
+        """
+        Returns true if the process is initialized and running.
+        """
+        return self.run_flag
         
     def process(self):
         """
         Perform one interation of the processing loop.
         """
-        for i in range(len(self.streams.streams)):     # TODO don't use 100% if no streams
+        for i in range(len(self.streams.streams)):     # TODO don't spin if no streams
             self.streams.streams[i].process()
+
+    def get_config(self):
+        """
+        Return a dict of the server's streams/models/datasets config
+        """
+        return {
+            "streams" : self.streams.get_config(),
+            "models" : [],  # TODO implement models/streams
+            "datasets" : []
+        }
