d6d7c5b75678306731d9f125fdcfc4e45173d303
Dustin Franklin <dustinf@nvidia.com>
2023-01-12 12:26:09 -0500

    renamed events.py

--- python/www/dash/layout/__init__.py
+++ python/www/dash/layout/__init__.py
@@ -4,10 +4,10 @@ from .grid import create_grid
 from .navbar import create_navbar
 from .alerts import create_alerts
 from .actions import create_actions_dialog
+from .events import create_event_table
 
 from .video_player import create_video_player
 from .stream_options import create_stream_dialog, create_stream_options
 from .model_options import create_model_dialog, create_model_options
-from .event_table import create_event_table
 
 #from .help import create_help
