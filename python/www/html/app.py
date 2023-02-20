dc8979e98ab75f5449ad64dbad074418de898b13
Dustin Franklin <dustinf@nvidia.com>
2023-02-19 22:17:31 -0500

    updated HTML example

--- python/www/html/app.py
+++ python/www/html/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2023, NVIDIA CORPORATION. All rights reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the 'Software'),
@@ -22,49 +22,61 @@
 #
 
 import threading
+import traceback
 import argparse
 import sys
 import ssl
 
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
-from jetson_inference import imageNet, detectNet
+from jetson_inference import imageNet, detectNet, actionNet
 from jetson_utils import videoSource, videoOutput
 
 
-class MediaThread(threading.Thread):
+class Stream(threading.Thread):
     def __init__(self, args):
         super().__init__()
         self.args = args
         self.input = videoSource(args.input, argv=sys.argv)
         self.output = videoOutput(args.output, argv=sys.argv)
         self.frames = 0
-        
+
+    def process(self):
+        img = self.input.Capture()
+        self.output.Render(img)
+        self.frames += 1
+        if self.frames % 25 == 0 or self.frames < 15:
+            print(f"captured {self.frames} frames from {args.input} => {args.output} ({img.width} x {img.height})")
+   
     def run(self):
         while True:
-            img = self.input.Capture()
-            self.output.Render(img)
-            self.frames += 1
-            if self.frames % 25 == 0 or self.frames < 15:
-                print(f"captured {self.frames} frames from {args.input} => {args.output} ({img.width} x {img.height})")
-	
-            
+            try:
+                self.process()
+            except:
+                traceback.print_exc()
+        
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, 
+                                     epilog=imageNet.Usage() + detectNet.Usage() + actionNet.Usage() + 
+                                     videoSource.Usage() + videoOutput.Usage())
 
     parser.add_argument("--host", default='0.0.0.0', type=str, help="interface for the webserver to use (default is all interfaces, 0.0.0.0)")
-    parser.add_argument("--port", default=8080, type=int, help="port used for webserver (default is 8050)")
+    parser.add_argument("--port", default=8050, type=int, help="port used for webserver (default is 8050)")
     parser.add_argument("--ssl-key", default='', type=str, help="path to PEM-encoded SSL/TLS key file for enabling HTTPS")
     parser.add_argument("--ssl-cert", default='', type=str, help="path to PEM-encoded SSL/TLS certificate file for enabling HTTPS")
-    parser.add_argument("--input", default='', type=str, help="input camera stream or video file (default: disabled)")
+    parser.add_argument("--input", default='webrtc://@:8554/input', type=str, help="input camera stream or video file")
     parser.add_argument("--output", default='webrtc://@:8554/output', type=str, help="WebRTC output stream to serve from --input")
+    parser.add_argument("--no-stream", action="store_true", help="disable creation of the input/output stream (serve website only)")
+    parser.add_argument("--classification", action="store_true", help="load classification model (see imageNet arguments)")
+    parser.add_argument("--detection", action="store_true", help="load object detection model (see detectNet arguments)")
+    parser.add_argument("--action", action="store_true", help="load action recognition model (see actionNet arguments)")
     
     args = parser.parse_known_args()[0]
     
-    # start media / AI thread
-    if args.input:
-        mediaThread = MediaThread(args)
-        mediaThread.start()
+    # start stream thread
+    if not args.no_stream:
+        stream = Stream(args)
+        stream.start()
 
     # patch to serve javascript
     SimpleHTTPRequestHandler.extensions_map['.js'] = 'text/javascript'
--- python/www/html/webrtc.js
+++ python/www/html/webrtc.js
@@ -1,140 +1,144 @@
+/*
+ * Copyright (c) 2023, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+var connections = {};
 
- var connections = {};
-
- function reportError(msg) {
-	console.log(msg);
- }
+function reportError(msg) {
+  console.log(msg);
+}
  
- function getLocalStream() {
-   var constraints = {'audio': false, 'video': true};
-   if (navigator.mediaDevices.getUserMedia) {
-	return navigator.mediaDevices.getUserMedia(constraints);
-   }
- }
+function getWebsocketProtocol() {
+  return window.location.protocol == 'https:' ? 'wss://' : 'ws://';
+}
 
- function onIncomingSDP(url, sdp) {
-   console.log('Incoming SDP: (%%s)' + JSON.stringify(sdp), url);
+function getWebsocketURL(name, port=8554) {
+  return `${getWebsocketProtocol()}${window.location.hostname}:${port}/${name}`;
+}
+  
+function checkMediaDevices() {
+  return (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia || !navigator.mediaDevices.enumerateDevices) ? false : true;
+}
 
-   function onLocalDescription(desc) {
-	console.log('Local description (%%s)\\n' + JSON.stringify(desc), url);
-	connections[url].webrtcPeer.setLocalDescription(desc).then(function() {
-	  connections[url].websocket.send(JSON.stringify({ type: 'sdp', 'data': connections[url].webrtcPeer.localDescription }));
-	}).catch(reportError);
-   }
+function onIncomingSDP(url, sdp) {
+  console.log('Incoming SDP: (%s)' + JSON.stringify(sdp), url);
 
-   connections[url].webrtcPeer.setRemoteDescription(sdp).catch(reportError);
+  function onLocalDescription(desc) {
+    console.log('Local description (%s)\n' + JSON.stringify(desc), url);
+    connections[url].webrtcPeer.setLocalDescription(desc).then(function() {
+      connections[url].websocket.send(JSON.stringify({ type: 'sdp', 'data': connections[url].webrtcPeer.localDescription }));
+    }).catch(reportError);
+  }
 
-   if( connections[url].type == 'inbound' ) {
-	connections[url].webrtcPeer.createAnswer().then(onLocalDescription).catch(reportError);
-   }
-   else if( connections[url].type == 'outbound' ) {
-	getLocalStream().then((stream) => {
-	  console.log('Adding local stream');
-	  connections[url].webrtcPeer.addStream(stream);
-	  connections[url].webrtcPeer.createAnswer().then(onLocalDescription).catch(reportError);
-	});
-   }
+  connections[url].webrtcPeer.setRemoteDescription(sdp).catch(reportError);
 
- }
+  if( connections[url].type == 'inbound' ) {
+    connections[url].webrtcPeer.createAnswer().then(onLocalDescription).catch(reportError);
+  }
+  else if( connections[url].type == 'outbound' ) {
+    var constraints = {'audio': false, 'video': { deviceId: connections[url].deviceId }};
+    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+      console.log('Adding local stream (deviceId=%s)', connections[url].deviceId);
+      connections[url].webrtcPeer.addStream(stream);
+      connections[url].webrtcPeer.createAnswer().then(onLocalDescription).catch(reportError);
+    }).catch(reportError);
+  }
+}
 
+function onIncomingICE(url, ice) {
+  var candidate = new RTCIceCandidate(ice);
+  console.log('Incoming ICE (%s)\n' + JSON.stringify(ice), url);
+  connections[url].webrtcPeer.addIceCandidate(candidate).catch(reportError);
+}
 
- function onIncomingICE(url, ice) {
-   var candidate = new RTCIceCandidate(ice);
-   console.log('Incoming ICE (%%s)\\n' + JSON.stringify(ice), url);
-   connections[url].webrtcPeer.addIceCandidate(candidate).catch(reportError);
- }
+function onAddRemoteStream(event) {
+  var url = event.srcElement.url;
+  console.log('Adding remote stream to HTML video player (%s)', url);
+  connections[url].videoElement.srcObject = event.streams[0];
+  connections[url].videoElement.play();
+}
 
+function onIceCandidate(event) {
+  var url = event.srcElement.url;
 
- function onAddRemoteStream(event) {
-   var url = event.srcElement.url;
-   console.log('Adding remote stream to HTML video player (%%s)', url);
-   connections[url].videoElement.srcObject = event.streams[0];
-   connections[url].videoElement.play();
- }
+  if (event.candidate == null)
+    return;
 
+  console.log('Sending ICE candidate out (%s)\n' + JSON.stringify(event.candidate), url);
+  connections[url].websocket.send(JSON.stringify({'type': 'ice', 'data': event.candidate }));
+}
 
- function onIceCandidate(event) {
-   var url = event.srcElement.url;
+function onServerMessage(event) {
+  var msg;
+  var url = event.srcElement.url;
 
-   if (event.candidate == null)
-	return;
+  try {
+    msg = JSON.parse(event.data);
+  } catch (e) {
+    return;
+  }
 
-   console.log('Sending ICE candidate out (%%s)\\n' + JSON.stringify(event.candidate), url);
-   connections[url].websocket.send(JSON.stringify({'type': 'ice', 'data': event.candidate }));
- }
+  if( !connections[url].webrtcPeer ) {
+    connections[url].webrtcPeer = new RTCPeerConnection(connections[url].webrtcConfig);
+    connections[url].webrtcPeer.url = url;
 
+    connections[url].webrtcPeer.onconnectionstatechange = (ev) => {
+      console.log('WebRTC connection state (%s) ' + connections[url].webrtcPeer.connectionState, url);
+    }
 
- function onServerMessage(event) {
-   var msg;
-   var url = event.srcElement.url;
+    if( connections[url].type == 'inbound' ) {
+      connections[url].webrtcPeer.ontrack = onAddRemoteStream;
+    }
+    
+    connections[url].webrtcPeer.onicecandidate = onIceCandidate;
+  }
 
-   try {
-	msg = JSON.parse(event.data);
-   } catch (e) {
-	return;
-   }
+  switch (msg.type) {
+    case 'sdp': onIncomingSDP(url, msg.data); break;
+    case 'ice': onIncomingICE(url, msg.data); break;
+    default: break;
+  }
+}
 
-   if( !connections[url].webrtcPeer ) {
-	connections[url].webrtcPeer = new RTCPeerConnection(connections[url].webrtcConfig);
-	connections[url].webrtcPeer.url = url;
-
-	connections[url].webrtcPeer.onconnectionstatechange = (ev) => {
-	  console.log('WebRTC connection state (%%s) ' + connections[url].webrtcPeer.connectionState, url);
-	}
-
-	if( connections[url].type == 'inbound' )
-	  connections[url].webrtcPeer.ontrack = onAddRemoteStream;
-	connections[url].webrtcPeer.onicecandidate = onIceCandidate;
-   }
-
-   switch (msg.type) {
-	case 'sdp': onIncomingSDP(url, msg.data); break;
-	case 'ice': onIncomingICE(url, msg.data); break;
-	default: break;
-   }
- }
-
- function playStream(url, videoElement) {
-   console.log('playing stream ' + url); 
+function playStream(url, videoElement) {
+  console.log('playing stream ' + url); 
    
-   connections[url] = {};
+  connections[url] = {};
 
-   connections[url].type = 'inbound';
-   connections[url].videoElement = videoElement;
-   connections[url].webrtcConfig = { 'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }] };
+  connections[url].type = 'inbound';
+  connections[url].videoElement = videoElement;
+  connections[url].webrtcConfig = { 'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }] };
    
-   connections[url].websocket = new WebSocket(url);
-   connections[url].websocket.addEventListener('message', onServerMessage);
- }
+  connections[url].websocket = new WebSocket(url);
+  connections[url].websocket.addEventListener('message', onServerMessage);
+}
 
- function sendStream(hostname, port, path, configuration, reportErrorCB) {
-   var l = window.location;
-   if( path == 'null' )
-	 return;
-   if( l.protocol != 'https:' ) {
-	 alert('Please use HTTPS to enable the use of your browser webcam');
-	 return;
-   }
-   if( !navigator.mediaDevices || !navigator.mediaDevices.getUserMedia ) {
-	 alert('getUserMedia() not available (confirm HTTPS is being used)');
-	 return;
-   }
-   var wsProt = (l.protocol == 'https:') ? 'wss://' : 'ws://';
-   var wsHost = (hostname != undefined) ? hostname : l.hostname;
-   var wsPort = (port != undefined) ? port : l.port;
-   var wsPath = (path != undefined) ? path : '/ws';
-   if (wsPort)
-	wsPort = ':' + wsPort;
-   var wsUrl = wsProt + wsHost + wsPort + wsPath;
-   console.log('Video server URL: ' + wsUrl);
-   var url = wsUrl;
+function sendStream(url, deviceId) {
+  console.log(`sending stream ${url}  (deviceId=${deviceId})`);
 
-   connections[url] = {};
+  connections[url] = {};
 
-   connections[url].type = 'outbound';
-   connections[url].webrtcConfig = configuration;
-   reportError = (reportErrorCB != undefined) ? reportErrorCB : function(text) {};
+  connections[url].type = 'outbound';
+  connections[url].deviceId = deviceId;
+  connections[url].webrtcConfig = { 'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }] };
 
-   connections[url].websocket = new WebSocket(wsUrl);
-   connections[url].websocket.addEventListener('message', onServerMessage);
- }
+  connections[url].websocket = new WebSocket(url);
+  connections[url].websocket.addEventListener('message', onServerMessage);
+}
